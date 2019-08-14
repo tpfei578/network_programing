8df3e20844dd88102bc84bfff9379d6a530c5df5
tpfei <tpfei609@gmail.com>
2019-08-14 12:47:19 +0800

    Rename old dir and Add Udp demo

---
+++ lib/common.h
@@ -0,0 +1,14 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <string.h>
+#define SERVER_PORT 12345
+#define MAXLINE 1024
+
+#endif
